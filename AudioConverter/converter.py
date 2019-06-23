85d8dae53e26e98dfc382416e8be8c7f8e3651ab
David Lam <dav_lam@ymail.com>
2019-06-23 05:12:45 -0700

    added classes, type hinting; applied black formatting

--- AudioConverter/converter.py
+++ AudioConverter/converter.py
@@ -1,73 +1,151 @@
 import pathlib
+from multiprocessing import Pool
+from typing import Sequence, Optional
+
 import click
 from pydub import AudioSegment
-from multiprocessing import Pool
 
-AUDIO_EXTENSIONS = ['.mp3', '.flac', '.aiff', '.mp4', '.m4a']
+AUDIO_EXTENSIONS = [".mp3", ".flac", ".aiff", ".mp4", ".m4a"]
+
+
+class Logger(object):
+    def success(self, message: str):
+        """
+        Display a success message in green.
+        """
+        self.display("[ SUCCESS ] {}".format(message), "green")
+
+    def info(self, message: str):
+        """
+        Display an info message in blue.
+        """
+        self.display("[ INFO    ] {}".format(message), "blue")
+
+    def verbose(self, message: str, verbose_flag: bool):
+        """
+        Display a verbose message in cyan.
+        """
+        if verbose_flag:
+            self.display("[ DEBUG   ] {}".format(message), "cyan")
+
+    def error(self, message: str):
+        """
+        Display an error message in red.
+        """
+        self.display("[ ERROR   ] {}".format(message), "red")
+
+    @staticmethod
+    def display(formatted_message, color):
+        """
+        Style a formatted_message message with the given color
+        and print it.
+        """
+        click.secho(formatted_message, fg=color)
+
+
+class Config(object):
+    """
+    CLI global configurations
+    """
+
+    __slots__ = ["verbose", "logger"]
+
+    def __init__(self, verbose: bool):
+        self.verbose = verbose
+        self.logger = Logger()
+
+
+class ConversionJob(object):
+    """
+    Details related to audio conversion jobs
+    """
+
+    __slots__ = ["output_format", "verbose", "output_path", "file_path", "logger"]
+
+    def __init__(
+        self,
+        output_format: str,
+        verbose: bool,
+        output_path: pathlib.Path,
+        file_path: pathlib.Path,
+        logger: Optional[Logger] = None,
+    ):
+        self.output_format = output_format
+        self.verbose = verbose
+        self.output_path = output_path
+        self.file_path = file_path
+        self.logger = logger if logger is not None else Logger()
 
 
 @click.group()
 @click.version_option()
-@click.option('--verbose', '-v', is_flag=True, help='Enable Verbose Logging')
+@click.option("--verbose", "-v", type=bool, is_flag=True, help="Enable Verbose Logging")
 @click.pass_context
-def cli(context, verbose):
+def cli(context: click.Context, verbose: bool):
     """
     AudioConverter CLI
     """
-    config = {'verbose': verbose}
-    context.obj = {
-        'config': config
-    }
+    context.obj = Config(verbose)
 
 
 @cli.command()
-@click.argument('input_directory', type=click.Path(exists=True))
-@click.argument('output_directory', type=click.Path())
-@click.option('--output-format', '-o', type=click.Choice(AUDIO_EXTENSIONS),
-              default='.mp3', help='Target output format')
-@click.pass_context
-def convert(context, input_directory, output_directory, output_format):
+@click.argument(
+    "input_directory",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=str),
+)
+@click.argument(
+    "output_directory", type=click.Path(file_okay=False, dir_okay=True, path_type=str)
+)
+@click.option(
+    "--output-format",
+    "-o",
+    type=click.Choice(AUDIO_EXTENSIONS),
+    default=".mp3",
+    help="Target output format",
+)
+@click.pass_obj
+def convert(
+    config: Config, input_directory: str, output_directory: str, output_format: str
+):
     """
     Convert Input Directory Audio to Output Directory Audio
     """
-    config = get_config(context)
-
-    info('Starting conversion of {}.'.format(input_directory))
+    logger = config.logger
+    logger.info("Starting conversion of {}.".format(input_directory))
 
     input_path = pathlib.Path(input_directory)
     output_path = pathlib.Path(output_directory)
 
-    verbose('Input : {}'.format(input_path.as_posix()), config['verbose'])
-    verbose('Output: {}'.format(output_path.as_posix()), config['verbose'])
-
-    # Verify arguements: input must be a directory and
-    # output must not be an existing file
-    if not input_path.is_dir():
-        error('Input path {} is not an existing directory'.format(input_path.as_posix()))
-        exit(1)
-    if output_path.is_file():
-        error('Output path {} is not a directory'.format(output_path.as_posix()))
-        exit(1)
+    logger.verbose("Input : {}".format(input_path.as_posix()), config.verbose)
+    logger.verbose("Output: {}".format(output_path.as_posix()), config.verbose)
 
     if not output_path.exists():
-        verbose('Creating output directory {}'.format(output_path.as_posix()),
-                config['verbose'])
+        logger.verbose(
+            "Creating output directory {}".format(output_path.as_posix()),
+            config.verbose,
+        )
         output_path.mkdir(exist_ok=True)
 
     audio_files = get_audio_files(input_path)
-    audio_files = list(map(lambda x: {
-                                'output_format': output_format,
-                                'verbose': config['verbose'],
-                                'output_path': output_path,
-                                'file_path': x
-                           }, audio_files))
+    audio_files = list(
+        map(
+            lambda file_path: ConversionJob(
+                output_format=output_format,
+                verbose=config.verbose,
+                output_path=output_path,
+                file_path=file_path,
+                logger=logger,
+            ),
+            audio_files,
+        )
+    )
     with Pool(processes=5) as worker:
         worker.map(converter, audio_files)
 
-    success('See {} for converted audio.'.format(output_directory))
+    logger.success("See {} for converted audio.".format(output_path.as_posix()))
 
 
-def get_audio_files(input_path):
+def get_audio_files(input_path: pathlib.Path) -> Sequence[pathlib.Path]:
     """
     Recursively get audio files within the input_path.
     """
@@ -76,11 +154,11 @@ def get_audio_files(input_path):
         if input_file.is_file() and input_file.suffix.lower() in AUDIO_EXTENSIONS:
             audio_files.append(input_file)
         elif input_file.is_dir() and not input_file.is_symlink():
-            audio_files += get_audio_files(input_file)
+            audio_files.extend(get_audio_files(input_file))
     return audio_files
 
 
-def converter(audio_datum):
+def converter(conversion_job: ConversionJob):
     """
     Multiprocessing worker function.
     Expects audio_datum to have keys:
@@ -92,67 +170,23 @@ def converter(audio_datum):
     Converts the audio file_path to the desired output_format of the same name
     in the output_path.
     """
+    logger = conversion_job.logger
+
     # Conversion specific data
-    output_format = audio_datum['output_format'][1:]
-    output_path = audio_datum['output_path']
-    verbose_flag = audio_datum['verbose']
+    output_format = conversion_job.output_format[1:]  # ignore "." prefix
+    output_path = conversion_job.output_path
+    verbose_flag = conversion_job.verbose
 
     # File specific data
-    audio_file = audio_datum['file_path']
-    audio_name = audio_file.name[:audio_file.name.rfind('.')]
-    converted_name = '{}.{}'.format(audio_name, output_format)
+    audio_file = conversion_job.file_path
+    audio_name = audio_file.name[: audio_file.name.rfind(".")]
+    converted_name = "{}.{}".format(audio_name, output_format)
 
-    verbose('Converting {} to {}'.format(audio_name, output_format),
-            verbose_flag)
-    audio = AudioSegment.from_file(audio_file.as_posix(),
-                                   audio_file.suffix[1:])
+    logger.verbose(
+        "Converting {} to {}".format(audio_name, output_format), verbose_flag
+    )
+    audio = AudioSegment.from_file(audio_file.as_posix(), audio_file.suffix[1:])
     output_name = output_path.joinpath(converted_name)
-    audio.export(output_name.as_posix(),
-                 format=output_format,
-                 bitrate='192k')
+    audio.export(output_name.as_posix(), format=output_format, bitrate="192k")
 
-    verbose('{} converted'.format(audio_name), verbose_flag)
-
-
-def get_config(context):
-    """
-    Fetch the config dictionary from the click.Context.
-    """
-    return context.obj['config']
-
-
-def success(message):
-    """
-    Display a success message in green.
-    """
-    display('[ SUCCESS ] {}'.format(message), 'green')
-
-
-def info(message):
-    """
-    Display an info message in blue.
-    """
-    display('[ INFO    ] {}'.format(message), 'blue')
-
-
-def verbose(message, verbose_flag):
-    """
-    Display a verbose message in cyan.
-    """
-    if verbose_flag:
-        display('[ DEBUG   ] {}'.format(message), 'cyan')
-
-
-def error(message):
-    """
-    Display an error message in red.
-    """
-    display('[ ERROR   ] {}'.format(message), 'red')
-
-
-def display(formatted_message, color):
-    """
-    Style a formatted_message message with the given color
-    and print it.
-    """
-    click.secho(formatted_message, fg=color)
+    logger.verbose("{} converted".format(audio_name), verbose_flag)
