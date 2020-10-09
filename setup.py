b9141b198aa07729b129fa44c3ded12fd4054dcb
David Lam <dav_lam@ymail.com>
2020-10-09 03:33:03 -0700

    switch to poetry

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author="lamdav",
     author_email="david.lam@lamdav.com",
     license="MIT",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     packages=find_packages(),
     entry_points={"console_scripts": ["audioconvert=AudioConverter.converter:cli"]},
     classifiers=[
@@ -27,16 +27,19 @@ setup(
         "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Multimedia :: Sound/Audio :: Conversion",
         "Topic :: Utilities",
     ],
-    install_requires=["click==7.1.2", "colorama==0.4.3", "pydub==0.24.0"],
-    extra_requires={
-        "dev": {"black==19.10b0", "isort==4.3.21", "pip-tools==5.3.0", "pur==5.3.0"}
-    },
+    install_requires=[
+        "click>=7.1.2,<8.0.0",
+        "colorama>=0.4.3,<1.0.0",
+        "pydub>=0.24.1,<1.0.0",
+    ],
+    extras_require={"dev": {"black>=20.8b1", "isort>=5.6.1", "pur>=5.3.0"}},
     keywords=["audioconverter audio converter CLI"],
     include_package_data=True,
     project_urls={
